da2a769190092dc9b726701a1cf2d46393a32962
yurii0419 <yurii.babii.work9521@ukr.net>
2022-09-17 01:59:27 +0300

    Adds isVerified status to auth token

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -6,12 +6,12 @@ const issuer = process.env.TOKEN_ISSUER
 const audience = process.env.TOKEN_AUDIENCE
 
 const generateToken = (user: TokenUser, type: TokenType, expiresIn = '30 days'): string => {
-  const { id, email, role, logoutTime, company } = user
+  const { id, email, role, logoutTime, company, isVerified } = user
 
   const customerId = company?.customerId ?? null
 
   const token = jwt.sign(JSON.parse(JSON.stringify(
-    { id, customerId, email, role, logoutTime, type }
+    { id, customerId, email, role, logoutTime, type, isVerified }
   )), secretKey, { expiresIn, issuer, audience })
 
   return token
