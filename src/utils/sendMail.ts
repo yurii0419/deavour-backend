eeb3eb1df2f66f595162c72f910b40408eb7a6ba
yurii0419 <yurii.babii.work9521@ukr.net>
2023-04-18 20:17:35 +0300

    Removes unused sendMail function

--- src/utils/sendMail.ts
+++ src/utils/sendMail.ts
@@ -7,27 +7,6 @@ const mailer = String(process.env.MAILER_EMAIL)
 
 const appName = String(process.env.APP_NAME)
 
-const sendMail = async (firstName: string, email: string, token: string, url: string, subject: string, text: string, sandboxMode: boolean): Promise<any> => {
-  const msg = {
-    to: email,
-    from: `${appName} <${mailer}>`,
-    subject: `${subject}`,
-    text: `Hello ${firstName},\n\nFollow this link to ${text} for your ${email} account\n${url}/?token=${token}\n\nIf you didn't ask to ${text}, you can ignore this email.\n\nThanks,\n\n${appName} team`,
-    mail_settings: {
-      sandbox_mode: {
-        enable: sandboxMode
-      }
-    }
-  }
-
-  try {
-    const info = await sgMail.send(msg)
-    return info
-  } catch (error: any) {
-    return error.response.body.errors[0].message
-  }
-}
-
 export const sendNotifierEmail = async (email: string, subject: string, message: string, bccStatus = true, html = '', sandboxMode: boolean): Promise<any> => {
   const msg: EmailMessage = {
     to: email,
@@ -53,5 +32,3 @@ export const sendNotifierEmail = async (email: string, subject: string, message:
     return error.response.body.errors[0].message
   }
 }
-
-export default sendMail
