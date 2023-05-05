494714942e9ed03dbbc8ca7d32116e51216223a1
yurii0419 <yurii.babii.work9521@ukr.net>
2023-05-05 22:28:19 +0300

    Adds functionality to update, delete legal terms

--- src/services/LegalTextService.ts
+++ src/services/LegalTextService.ts
@@ -1,23 +1,60 @@
 import { v1 as uuidv1 } from 'uuid'
+import { Op } from 'sequelize'
 import BaseService from './BaseService'
 import db from '../models'
+
+function generateFilterQuery (filter: object): any {
+  const filterQuery: object = {}
+
+  Object.entries(filter).forEach(([key, value]) => {
+    if (value !== undefined && value !== '') {
+      filterQuery[`${key}`] = {
+        [Op.eq]: `${String(value)}`
+      }
+    }
+  })
+
+  return filterQuery
+}
 
 class LegalTextService extends BaseService {
   async insert (data: any): Promise<any> {
     const { company, legalText } = data
 
-    const response = await db[this.model].create({ ...legalText, id: uuidv1(), companyId: company.id })
+    const response = await db[this.model].create({ ...legalText, id: uuidv1(), companyId: company?.id })
 
     return response.toJSONFor(company)
   }
 
-  async getAllForCompany (limit: number, offset: number, companyId: string): Promise<any> {
+  async getAll (limit: number, offset: number, filter = { type: '' }): Promise<any> {
+    const where = generateFilterQuery(filter)
+
     const records = await db[this.model].findAndCountAll({
       limit,
       offset,
       order: [['createdAt', 'DESC']],
       attributes: { exclude: [] },
       where: {
+        ...where,
+        companyId: null
+      }
+    })
+
+    return {
+      count: records.count,
+      rows: records.rows.map((record: any) => record.toJSONFor())
+    }
+  }
+
+  async getAllForCompany (limit: number, offset: number, companyId: string, filter = { type: '' }): Promise<any> {
+    const where = generateFilterQuery(filter)
+    const records = await db[this.model].findAndCountAll({
+      limit,
+      offset,
+      order: [['createdAt', 'DESC']],
+      attributes: { exclude: [] },
+      where: {
+        ...where,
         companyId
       }
     })
