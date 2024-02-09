f6362cd50e4fcc943047a5f8c60e7a6ca1544cec
yurii0419 <yurii.babii.work9521@ukr.net>
2024-02-09 16:22:18 +0300

    Adds isDomainVerified on company object in user

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -16,7 +16,7 @@ const passportAuth = (passport: PassportStatic): any => {
     include: [
       {
         model: db.Company,
-        attributes: ['id', 'customerId', 'name', 'email', 'phone', 'vat', 'domain'],
+        attributes: ['id', 'customerId', 'name', 'email', 'phone', 'vat', 'domain', 'isDomainVerified'],
         as: 'company',
         include: [
           {
