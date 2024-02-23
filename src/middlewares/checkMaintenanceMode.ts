ed256cf16a2648981eaa74ccda1f18552b36d6db
yurii0419 <yurii.babii.work9521@ukr.net>
2024-02-23 13:06:03 +0300

    Updates maintenance mode message

--- src/middlewares/checkMaintenanceMode.ts
+++ src/middlewares/checkMaintenanceMode.ts
@@ -3,8 +3,10 @@ import localizedFormat from 'dayjs/plugin/localizedFormat'
 import * as statusCodes from '../constants/statusCodes'
 import MaintenanceModeService from '../services/MaintenanceModeService'
 import type { CustomNext, CustomRequest, CustomResponse, IMaintenanceMode } from '../types'
+import relativeTime from 'dayjs/plugin/relativeTime'
 
 dayjs.extend(localizedFormat)
+dayjs.extend(relativeTime)
 
 const maintenanceModeService = new MaintenanceModeService('MaintenanceMode')
 const checkMaintenanceMode = async (req: CustomRequest, res: CustomResponse, next: CustomNext): Promise<any> => {
@@ -24,7 +26,7 @@ const checkMaintenanceMode = async (req: CustomRequest, res: CustomResponse, nex
       statusCode: statusCodes.SERVICE_UNAVAILABLE,
       success: false,
       errors: {
-        message: `Service unavailable from ${dayjs(inMaintenance.startDate).format('lll').toString()} to ${dayjs(inMaintenance.endDate).format('lll').toString()}: ${inMaintenance.reason}`
+        message: `Service unavailable and will be back ${dayjs(now).to(inMaintenance.endDate)}: ${inMaintenance.reason}`
       }
     })
   }
