6d20d1c8d6e164bbcfd5b6a2d9b964e0cca8457d
yurii0419 <yurii.babii.work9521@ukr.net>
2025-01-21 21:46:26 +0100

    fix: gcp private key issue [Start: #188746560]

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,6 +1,6 @@
 import { Storage } from '@google-cloud/storage'
 
-const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n')
+const privateKey = process.env.FIREBASE_PRIVATE_KEY?.split(String.raw`\n`).join('\n')
 
 const storage = new Storage(
   {
