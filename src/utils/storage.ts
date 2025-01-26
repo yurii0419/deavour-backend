b2829be8b83dafc4abc327a7fa5f9f2b703298c5
yurii0419 <yurii.babii.work9521@ukr.net>
2025-01-26 03:13:47 +0300

    Removes duplicate dependency

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,20 +1,9 @@
-import { Storage } from '@google-cloud/storage'
+import { getStorage } from 'firebase-admin/storage'
 
-const privateKey = (process.env.FIREBASE_PRIVATE_KEY as string).split(String.raw`\n`).join('\n')
-
-const storage = new Storage(
-  {
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    credentials: {
-      client_email: process.env.FIREBASE_CLIENT_EMAIL,
-      private_key: privateKey
-    }
-  }
-)
-const bucketName = String(process.env.GETEC_STORAGE_BUCKET)
+const storageBucket = String(process.env.GETEC_STORAGE_BUCKET)
+const bucket = getStorage().bucket(storageBucket)
 
 export const uploadToGCS = async (fileName: string, fileBuffer: Buffer): Promise<string> => {
-  const bucket = storage.bucket(bucketName)
   const file = bucket.file(fileName)
 
   await file.save(fileBuffer, {
@@ -23,5 +12,5 @@ export const uploadToGCS = async (fileName: string, fileBuffer: Buffer): Promise
     }
   })
 
-  return `https://storage.cloud.google.com/${bucketName}/${fileName}`
+  return `https://storage.cloud.google.com/${storageBucket}/${fileName}`
 }
