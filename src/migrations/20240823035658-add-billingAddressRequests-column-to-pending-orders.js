c9b9e6522d80a40eda3e66e3ce383e620b762540
yurii0419 <yurii.babii.work9521@ukr.net>
2025-02-07 18:58:02 +0300

    Updates billing address requests to jsonb

--- src/migrations/20240823035658-add-billingAddressRequests-column-to-pending-orders.js
+++ src/migrations/20240823035658-add-billingAddressRequests-column-to-pending-orders.js
@@ -4,7 +4,7 @@ module.exports = {
     return Promise.all([
       queryInterface.addColumn('PendingOrders', 'billingAddressRequests', {
         allowNull: true,
-        type: Sequelize.JSON
+        type: Sequelize.JSONB
       })
     ])
   },
