2be0f2d2abb8963c9fbb8217af6cde7f5d5bb6c2
yurii0419 <yurii.babii.work9521@ukr.net>
2025-02-15 11:43:59 +0300

    Simplifies routes imports in app.ts file

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHt
 import http from 'http'
 import joiErrors from './middlewares/joiErrors'
 import * as statusCodes from './constants/statusCodes'
-import routers from './routes'
+import routes, { maintenanceModeBypassRoutes } from './routes'
 import passportAuth from './config/passport'
 import checkMaintenanceMode from './middlewares/checkMaintenanceMode'
 import resolvers from './graphql/resolvers'
@@ -38,73 +38,15 @@ app.use(express.json({ limit }))
 app.use(express.urlencoded({ extended: false, limit, parameterLimit: 2000000 }))
 
 // Routes that bypass maintenance mode
-app.use(apiPrefix, routers.maintenanceModeRouter())
-app.use(authPrefix, routers.authRouter())
-app.use(apiPrefix, routers.webhookRouter())
+app.use(apiPrefix, maintenanceModeBypassRoutes.maintenanceModeRouter())
+app.use(authPrefix, maintenanceModeBypassRoutes.authRouter())
+app.use(apiPrefix, maintenanceModeBypassRoutes.webhookRouter())
 
 // Middleware to check for maintenance mode
 app.use(checkMaintenanceMode)
 
 // Add routes to the app
-app.use(apiPrefix, routers.userRouter())
-app.use(apiPrefix, routers.profileRouter())
-app.use(apiPrefix, routers.companyRouter())
-app.use(apiPrefix, routers.addressRouter())
-app.use(apiPrefix, routers.recipientRouter())
-app.use(apiPrefix, routers.campaignRouter())
-app.use(apiPrefix, routers.salutationRouter())
-app.use(apiPrefix, routers.bundleRouter())
-app.use(apiPrefix, routers.bundleItemRouter())
-app.use(apiPrefix, routers.pictureRouter())
-app.use(apiPrefix, routers.shipmentRouter())
-app.use(apiPrefix, routers.costCenterRouter())
-app.use(apiPrefix, routers.productRouter())
-app.use(apiPrefix, routers.orderRouter())
-app.use(apiPrefix, routers.secondaryDomainRouter())
-app.use(apiPrefix, routers.legalTextRouter())
-app.use(apiPrefix, routers.privacyRuleRouter())
-app.use(apiPrefix, routers.accessPermissionsRouter())
-app.use(apiPrefix, routers.healthcheckRouter())
-app.use(apiPrefix, routers.shippingMethodRouter())
-app.use(apiPrefix, routers.pendingOrderRouter())
-app.use(apiPrefix, routers.cardTemplateRouter())
-app.use(apiPrefix, routers.greetingCardRouter())
-app.use(apiPrefix, routers.campaignShippingDestinationRouter())
-app.use(apiPrefix, routers.campaignOrderLimitRouter())
-app.use(apiPrefix, routers.emailTemplateRouter())
-app.use(apiPrefix, routers.emailTempleTypeRouter())
-app.use(apiPrefix, routers.campaignAddressRouter())
-app.use(apiPrefix, routers.productCategoryRouter())
-app.use(apiPrefix, routers.productCategoryTagRouter())
-app.use(apiPrefix, routers.productGraduatedPriceRouter())
-app.use(apiPrefix, routers.productColorRouter())
-app.use(apiPrefix, routers.productMaterialRouter())
-app.use(apiPrefix, routers.productSizeRouter())
-app.use(apiPrefix, routers.productAccessControlGroupRouter())
-app.use(apiPrefix, routers.companyInProductAccessControlGroupRouter())
-app.use(apiPrefix, routers.userInProductAccessControlGroupRouter())
-app.use(apiPrefix, routers.productCategoryTagInProductAccessControlGroupRouter())
-app.use(apiPrefix, routers.companyUserGroupRouter())
-app.use(apiPrefix, routers.userInCompanyUserGroupRouter())
-app.use(apiPrefix, routers.companyUserGroupInProductAccessControlGroupRouter())
-app.use(apiPrefix, routers.taxRateRouter())
-app.use(apiPrefix, routers.massUnitRouter())
-app.use(apiPrefix, routers.salesUnitRouter())
-app.use(apiPrefix, routers.productInProductCategoryRouter())
-app.use(apiPrefix, routers.invoiceRouter())
-app.use(apiPrefix, routers.inboundRouter())
-app.use(apiPrefix, routers.outboundRouter())
-app.use(apiPrefix, routers.itemRouter())
-app.use(apiPrefix, routers.categoryRouter())
-app.use(apiPrefix, routers.campaignQuotaRouter())
-app.use(apiPrefix, routers.campaignQuotaNotificationRouter())
-app.use(apiPrefix, routers.apiKeyRouter())
-app.use(apiPrefix, routers.orderConfirmationRouter())
-app.use(apiPrefix, routers.packingSlipRouter())
-app.use(apiPrefix, routers.titleRouter())
-app.use(apiPrefix, routers.campaignAdditionalProductSettingRouter())
-app.use(apiPrefix, routers.productCustomisationRouter())
-app.use(apiPrefix, routers.productStockNotificationRouter())
+app.use(apiPrefix, routes)
 
 // Add validation middleware
 app.use(joiErrors)
@@ -122,7 +64,7 @@ app.get('/favicon.ico', (req: Request, res: Response) => {
   res.status(statusCodes.OK).sendFile(fileName, options)
 })
 
-app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+app.use((err: Error & { status?: number }, req: Request, res: Response, next: NextFunction) => {
   if (err.status === statusCodes.PAYLOAD_TOO_LARGE) {
     res.status(statusCodes.PAYLOAD_TOO_LARGE).send({
       statusCode: statusCodes.PAYLOAD_TOO_LARGE,
@@ -132,6 +74,7 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
       }
     })
   }
+  next(err)
 })
 
 export const appHttpServer = http.createServer(app)
