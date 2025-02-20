76a536b54f94d9cf8b4ee53da3c3904aaaa07309
yurii0419 <yurii.babii.work9521@ukr.net>
2025-02-20 20:57:40 +0100

    fix: test case

--- src/tests/integration/authRouter.spec.ts
+++ src/tests/integration/authRouter.spec.ts
@@ -798,7 +798,7 @@ describe('Auth Actions', () => {
     })
 
     it('should return 403 when the magic link has expired and has not been set in the env', async () => {
-      process.env.MAGIC_LINK_EXPIRATION = undefined
+      delete process.env.MAGIC_LINK_EXPIRATION
       const resLogin = await chai
         .request(app)
         .post('/auth/login')
